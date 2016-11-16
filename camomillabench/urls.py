8ba4efcf762a9679baf4d6906bd1697ee4d2d5b9
Andrea Stagi <stagi.andrea@gmail.com>
2016-11-16 11:09:40 +0100

    Add deploy info

--- camomillabench/urls.py
+++ camomillabench/urls.py
@@ -14,3 +14,10 @@ urlpatterns = [
     url(r'^api-token-auth/', CamomillaObtainAuthToken.as_view()),
     url(r'^api/', include('camomilla.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+try:
+    urlpatterns = [
+        url(r'^{0}/'.format(settings.ULR_PREFIX), include(urlpatterns)),
+    ]
+except AttributeError as ex:
+    pass
--- camomillabench/wsgi.py
+++ camomillabench/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "camomilla.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "camomillabench.settings")
 
 application = get_wsgi_application()
