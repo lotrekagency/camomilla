dd7dae0e074869cc6f2ec7bc8243d559abc34deb
Andrea Stagi <stagi.andrea@gmail.com>
2016-11-24 11:08:09 +0100

    Let Gunicorn running with a socket 🎉

--- deploy_settings_example.sh
+++ deploy_settings_example.sh
@@ -4,10 +4,13 @@ PYTHON_PATH='/usr/local/bin/python3.5'
 ADMIN_URL_SUFFIX='van_halen_1984'
 
 # Gunicorn settings
-export WORKERS=6
-export HOST='0.0.0.0'
-export PORT=8090
+# By default gunicorn will be communicating through a local socket
+# but you can specify an host and a port if you want
+#
+# export HOST='0.0.0.0'
+# export PORT=8090
+# export WORKERS=6
 
 # Here you can export variables and do other things if needed
-# e.g.
+#
 # export LDFLAGS="-I/usr/local/opt/openssl/include -L/usr/local/opt/openssl/lib"
