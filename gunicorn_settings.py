122b01e0a422818b861d8ce836e1735d6012bc5e
Andrea Stagi <stagi.andrea@gmail.com>
2017-03-06 03:10:22 +0100

    Keep only the app in this repo

--- gunicorn_settings.py
+++
@@ -1,8 +0,0 @@
-import multiprocessing
-import os
-
-if not 'HOST' in os.environ and not 'PORT' in 'HOST':
-    bind = "unix:{0}".format(os.environ.get('SOCKET', 'gunicorn.socket'))
-else:
-    bind = "{0}:{1}".format(os.environ.get('HOST', '127.0.0.1'), os.environ.get('PORT', '8080'))
-workers = os.environ.get('WORKERS', multiprocessing.cpu_count() * 2 + 1)
