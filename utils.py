ab69333d9be5a2c55cf920a5b946158889ad16dd
sᴀʟᴠᴀᴛᴏʀᴇ <svt.busiello@gmail.com>
2017-03-10 01:20:14 +0100

    Updated SEO Utils Engine Hotfix for multilanguage site, and custom SitemapUrl class

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 import urllib.parse
-
+from django.utils.translation import get_language
 from .models import SitemapUrl
 
 
@@ -13,18 +13,24 @@ def get_complete_url(request, url, language=''):
     return complete_url
 
 
-def get_seo(request, page_requested):
+
+def get_seo(request, page_requested, lang='', model=SitemapUrl):
+    if not lang:
+        lang = get_language()
     try:
-        meta_tag = SitemapUrl.objects.language().get(page=page_requested)
+        meta_tag = model.objects.language().get(page=page_requested)
         if not meta_tag.og_title:
             meta_tag.og_title = meta_tag.title
         if not meta_tag.og_description:
             meta_tag.og_description = meta_tag.description
-        meta_tag.canonical = get_complete_url(request, meta_tag.canonical)
+        if not meta_tag.canonical:
+            meta_tag.canonical = get_complete_url(request, meta_tag.permalink,lang)
+        else:
+            meta_tag.canonical = get_complete_url(request, meta_tag.canonical,lang)
         if not meta_tag.og_url:
             meta_tag.og_url = meta_tag.canonical
         else:
-            meta_tag.og_url = get_complete_url(request, meta_tag.og_url)
+            meta_tag.og_url = get_complete_url(request, meta_tag.og_url,lang)
         return meta_tag
-    except SitemapUrl.DoesNotExist:
+    except model.DoesNotExist:
         return None
