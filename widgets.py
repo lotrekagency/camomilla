087e0000eb55b9f795917990521dabaa1c756469
Andrea Stagi <stagi.andrea@gmail.com>
2017-03-13 02:41:32 +0100

    Add custom widgets for media selection

---
+++ widgets.py
@@ -0,0 +1,34 @@
+from django.forms import CheckboxSelectMultiple
+
+from django.template.loader import render_to_string
+from django.utils.safestring import mark_safe
+
+from camomilla.models import Media
+
+
+class MediaSelectMultiple(CheckboxSelectMultiple):
+
+    template_name = 'camomilla/widgets/checkbox.html'
+
+    def _build_choices(self, name, all_choices, choices_selected):
+        choices = []
+        for choice in all_choices:
+            thumb_url = ''
+            media_obj = Media.objects.get(id=choice[0])
+            if media_obj.is_image and media_obj.thumbnail:
+                thumb_url = media_obj.thumbnail.url
+            choices.append({
+                'thumb': thumb_url,
+                'value': choice[0],
+                'label': choice[1],
+                'selected': True if choice[0] in choices_selected else False
+            })
+        return choices
+
+    def render(self, name, value, attrs=None, choices=()):
+        choices = self._build_choices(name, self.choices, value)
+        html = render_to_string(
+            self.template_name,
+            {'name': name, 'choices': choices}
+        )
+        return mark_safe(html)
