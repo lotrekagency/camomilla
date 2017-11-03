d5f401b0e9ff6424223fbd8eb0409bbbac2911ea
Andrea Stagi <stagi.andrea@gmail.com>
2017-11-03 22:15:02 +0100

    Fix cache middleware

--- middleware/cache_camomilla.py
+++ middleware/cache_camomilla.py
@@ -3,11 +3,16 @@ from django.middleware.cache import FetchFromCacheMiddleware
 
 class CacheCamomillaMiddleware(FetchFromCacheMiddleware):
     def process_request(self, request):
+        print ('middleware')
         if request.META.get('HTTP_DISABLE_CACHE', ''):
+            request._cache_update_cache = False
             return None
         if request.user and request.user.is_superuser:
+            print ('cache super')
+            request._cache_update_cache = False
             return None
         if request.user and request.user.is_authenticated():
             if request.user.level == '3' or request.user.level == '2':
+                request._cache_update_cache = False
                 return None
         return super(CacheCamomillaMiddleware, self).process_request(request)
