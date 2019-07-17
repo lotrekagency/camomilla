87c0ccb287e1af6d6132e89ac9be20205eb548b3
Andrea Stagi <stagi.andrea@gmail.com>
2019-07-17 04:09:26 +0200

    Remove useless stuff

--- camomilla/middleware/cache_camomilla.py
+++ camomilla/middleware/cache_camomilla.py
@@ -7,12 +7,8 @@ class CacheCamomillaMiddleware(FetchFromCacheMiddleware):
         if request.META.get('HTTP_DISABLE_CACHE', ''):
             request._cache_update_cache = False
             return None
-        if request.user and request.user.is_superuser:
+        if request.user and request.user.is_authenticated and request.user.is_superuser:
             print ('cache super')
             request._cache_update_cache = False
             return None
-        if request.user and request.user.is_authenticated():
-            if request.user.level == '3' or request.user.level == '2':
-                request._cache_update_cache = False
-                return None
         return super(CacheCamomillaMiddleware, self).process_request(request)
