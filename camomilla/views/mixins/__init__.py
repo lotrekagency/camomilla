36c2e8036bdc7e6ec27b726799332470897661c2
bnznamco <gabriele.baldi.01@gmail.com>
2021-05-10 11:02:57 +0200

    better translation initializaion

--- camomilla/views/mixins/__init__.py
+++ camomilla/views/mixins/__init__.py
@@ -4,23 +4,37 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from .pagination import *
 from ...permissions import CamomillaBasePermissions
+from django.utils import translation
 
 
 class GetUserLanguageMixin(object):
-    def _get_user_language(self):
-        return self.request.GET.get(
-            'language', self.request.data.get(
-                'language_code', settings.LANGUAGE_CODE
-            )
+    def _get_user_language(self, request):
+        self.active_language = request.GET.get(
+            "language",
+            request.data.get(
+                "language_code", translation.get_language_from_request(request)
+            ),
         )
+        self.language_fallbacks = True
+        if (
+            len(self.active_language.split("-")) == 2
+            and self.active_language.split("-")[0] == "nofallbacks"
+        ):
+            self.language_fallbacks = False
+            self.active_language = self.active_language.split("-")[1]
+        translation.activate(self.active_language)
+        return self.active_language
+
+    def initialize_request(self, request, *args, **kwargs):
+        self._get_user_language(request)
+        return super().initialize_request(request, *args, **kwargs)
 
     def get_queryset(self):
-        user_language = self._get_user_language()
-        fallbacks = True
-        if len(user_language.split('-')) == 2 and user_language.split('-')[0] == 'nofallbacks':
-            fallbacks = False
-            user_language = user_language.split('-')[1]
-        return self.model.objects.language(user_language).fallbacks().all() if fallbacks else self.model.objects.language(user_language).all()
+        return (
+            self.model.objects.language(self.active_language).fallbacks().all()
+            if self.language_fallbacks
+            else self.model.objects.language(self.active_language).all()
+        )
 
 
 class BulkDeleteMixin(object):
