c4fd330fb06dc3d49de4100865376a9eedb3f6f6
bnznamco <gabriele.baldi.01@gmail.com>
2021-12-02 15:46:15 +0100

    fix: fix articles and pages serializers requiring RelatedField fields.

--- camomilla/serializers/article.py
+++ camomilla/serializers/article.py
@@ -18,9 +18,9 @@ class CategorySerializer(BaseTranslatableModelSerializer):
 
 class ArticleSerializer(BaseTranslatableModelSerializer):
 
-    highlight_image = RelatedField(serializer=MediaSerializer, allow_null=True)
-    tags = RelatedField(serializer=TagSerializer, many=True, allow_null=True)
-    og_image = RelatedField(serializer=MediaSerializer, allow_null=True)
+    highlight_image = RelatedField(serializer=MediaSerializer, required=False, allow_null=True)
+    tags = RelatedField(serializer=TagSerializer, many=True, required=False, allow_null=True)
+    og_image = RelatedField(serializer=MediaSerializer, required=False, allow_null=True)
     
     class Meta:
         model = Article
