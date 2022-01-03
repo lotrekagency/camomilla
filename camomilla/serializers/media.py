8d6371ff94e460f2e84efde45882d7d192b7cce6
bnznamco <gabriele.baldi.01@gmail.com>
2022-01-03 14:56:35 +0100

    feat(media): added option to override Media file on update from api to maintain same url

--- camomilla/serializers/media.py
+++ camomilla/serializers/media.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from ..models import Media, MediaFolder
+from ..storages import OverwriteStorage
 from .base import BaseTranslatableModelSerializer
 
 
@@ -34,6 +35,17 @@ class MediaSerializer(BaseTranslatableModelSerializer):
                         }
                     )
         return result
+    
+    def update(self, instance, data):
+        same_url = self.initial_data.get("same_url", False)
+        if same_url:
+            new_file = data.pop("file", None)
+            if new_file:
+                instance = super().update(instance, data)
+                instance.file.storage = OverwriteStorage()
+                instance.file.save(instance.file.name, new_file, save=True)
+                return instance
+        return super().update(instance, data)
 
 
 class MediaFolderSerializer(BaseTranslatableModelSerializer):
