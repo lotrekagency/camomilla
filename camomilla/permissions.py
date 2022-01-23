0f5b824c0c614d9ec8fbb1ffd04644e3d30e774c
bnznamco <gabriele.baldi.01@gmail.com>
2022-01-23 12:53:51 +0100

    feat(api): brand new serializators views and permissions for users endpoint

--- camomilla/permissions.py
+++ camomilla/permissions.py
@@ -40,3 +40,7 @@ class CamomillaSuperUser(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return request.user.is_authenticated and request.user.is_superuser
+
+class ReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.method in permissions.SAFE_METHODS
