3556d33c7343208b9f7b0e380bb1614c24298169
bnznamco <gabriele.baldi.01@gmail.com>
2022-02-12 18:43:48 +0100

    fix: integrate Camomilla fields in Base model serializers

--- camomilla/serializers/base/__init__.py
+++ camomilla/serializers/base/__init__.py
@@ -1,13 +1,25 @@
 from ..mixins import LangInfoMixin, SetupEagerLoadingMixin, TranslationSetMixin
 from rest_framework import serializers
 from hvad.contrib.restframework import TranslatableModelSerializer
+from django.db import models
+from ..fields import FileField, ImageField, RelatedField
 
 
 class BaseModelSerializer(SetupEagerLoadingMixin, serializers.ModelSerializer):
-    pass
+    serializer_field_mapping = {
+        **serializers.ModelSerializer.serializer_field_mapping,
+        models.FileField: FileField,
+        models.ImageField: ImageField,
+    }
+    serializer_related_field = RelatedField
 
 
 class BaseTranslatableModelSerializer(
     LangInfoMixin, TranslationSetMixin, TranslatableModelSerializer
 ):
-    pass
+    serializer_field_mapping = {
+        **TranslatableModelSerializer.serializer_field_mapping,
+        models.FileField: FileField,
+        models.ImageField: ImageField,
+    }
+    serializer_related_field = RelatedField
