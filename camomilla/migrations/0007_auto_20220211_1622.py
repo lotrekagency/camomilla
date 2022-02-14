a811c72de2fa37c296215f41dec755405edbf28f
bnznamco <gabriele.baldi.01@gmail.com>
2022-02-14 11:12:24 +0100

    chore: black .

--- camomilla/migrations/0007_auto_20220211_1622.py
+++ camomilla/migrations/0007_auto_20220211_1622.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('camomilla', '0006_auto_20220103_1845'),
+        ("camomilla", "0006_auto_20220103_1845"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='articletranslation',
-            name='permalink',
+            model_name="articletranslation",
+            name="permalink",
             field=models.SlugField(max_length=200),
         ),
     ]
