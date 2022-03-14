dba13f4b89c5c219c6c765e79066c2b210a89cd3
bnznamco <gabriele.baldi.01@gmail.com>
2022-03-14 15:54:45 +0100

    chore: black .

--- camomilla/migrations/0008_auto_20220309_1616.py
+++ camomilla/migrations/0008_auto_20220309_1616.py
@@ -7,50 +7,54 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('camomilla', '0007_auto_20220211_1622'),
+        ("camomilla", "0007_auto_20220211_1622"),
     ]
 
     operations = [
         migrations.AlterModelOptions(
-            name='article',
-            options={'ordering': ['ordering']},
+            name="article",
+            options={"ordering": ["ordering"]},
         ),
         migrations.AlterModelOptions(
-            name='category',
-            options={'ordering': ['ordering'], 'verbose_name_plural': 'categories'},
+            name="category",
+            options={"ordering": ["ordering"], "verbose_name_plural": "categories"},
         ),
         migrations.AlterModelOptions(
-            name='page',
-            options={'ordering': ['ordering'], 'verbose_name': 'Page', 'verbose_name_plural': 'Pages'},
+            name="page",
+            options={
+                "ordering": ["ordering"],
+                "verbose_name": "Page",
+                "verbose_name_plural": "Pages",
+            },
         ),
         migrations.AddField(
-            model_name='article',
-            name='meta',
+            model_name="article",
+            name="meta",
             field=camomilla.fields.JSONField(default=dict),
         ),
         migrations.AddField(
-            model_name='article',
-            name='ordering',
+            model_name="article",
+            name="ordering",
             field=models.PositiveIntegerField(default=0),
         ),
         migrations.AddField(
-            model_name='category',
-            name='meta',
+            model_name="category",
+            name="meta",
             field=camomilla.fields.JSONField(default=dict),
         ),
         migrations.AddField(
-            model_name='category',
-            name='ordering',
+            model_name="category",
+            name="ordering",
             field=models.PositiveIntegerField(default=0),
         ),
         migrations.AddField(
-            model_name='page',
-            name='meta',
+            model_name="page",
+            name="meta",
             field=camomilla.fields.JSONField(default=dict),
         ),
         migrations.AddField(
-            model_name='page',
-            name='ordering',
+            model_name="page",
+            name="ordering",
             field=models.PositiveIntegerField(default=0),
         ),
     ]
