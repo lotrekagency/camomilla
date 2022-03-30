538e709dcc8fd2799b4063c265687b2e0315c076
bnznamco <gabriele.baldi.01@gmail.com>
2022-03-30 17:59:33 +0200

    fix: fix drf session authentication

--- camomilla/defaults.py
+++ camomilla/defaults.py
@@ -25,7 +25,7 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.TokenAuthentication"
-        "rest_framework.authentication.SessionAuthentication"
+        "camomilla.authentication.SessionAuthentication"
     ),
 }
 
