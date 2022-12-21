e83586eba127e970b714d78b2cb0ff353731d6fc
bnznamco <gabriele.baldi.01@gmail.com>
2022-12-21 11:53:10 +0100

    fix: fix recursive nestmixin functions

--- camomilla/serializers/base/__init__.py
+++ camomilla/serializers/base/__init__.py
@@ -1,5 +1,4 @@
 from hvad.contrib.restframework import TranslatableModelSerializer
-from hvad.models import TranslatableModel
 from rest_framework import serializers
 
 from ..fields import FieldsOverrideMixin, TranslatableFieldsOverrideMixin
@@ -13,36 +12,6 @@ from ..mixins import (
 )
 
 
-def build_standard_model_serializer(self, model, depth):
-    bases = (
-        NestMixin,
-        FieldsOverrideMixin,
-        JSONFieldPatchMixin,
-        OrderingMixin,
-        SetupEagerLoadingMixin,
-        serializers.ModelSerializer,
-    )
-    if issubclass(model, TranslatableModel):
-        bases = (
-            NestMixin,
-            TranslatableFieldsOverrideMixin,
-            JSONFieldPatchMixin,
-            OrderingMixin,
-            TranslatableModelSerializer,
-        )
-    return type(
-        f"{model.__name__}StandardSerializer",
-        bases,
-        {
-            "Meta": type(
-                "Meta",
-                (object,),
-                {"model": model, "depth": depth, "fields": "__all__"},
-            )
-        },
-    )
-
-
 class BaseModelSerializer(
     NestMixin,
     FieldsOverrideMixin,
@@ -51,8 +20,7 @@ class BaseModelSerializer(
     SetupEagerLoadingMixin,
     serializers.ModelSerializer,
 ):
-
-    build_standard_model_serializer = build_standard_model_serializer
+    pass
 
 
 class BaseTranslatableModelSerializer(
@@ -64,4 +32,4 @@ class BaseTranslatableModelSerializer(
     TranslationSetMixin,
     TranslatableModelSerializer,
 ):
-    build_standard_model_serializer = build_standard_model_serializer
+    pass
