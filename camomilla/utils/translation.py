7185ae17868a7826369790bbbf4f1230bd7f4a5d
bnznamco <gabriele.baldi.01@gmail.com>
2023-05-02 10:30:43 +0200

    feat: added some translations utils

--- camomilla/utils/translation.py
+++ camomilla/utils/translation.py
@@ -1,8 +1,9 @@
+import re
 from typing import Any, Iterable, Iterator
 
 from django.db.models import Model
 from django.utils.translation.trans_real import activate, get_language
-from modeltranslation.settings import AVAILABLE_LANGUAGES
+from modeltranslation.settings import AVAILABLE_LANGUAGES, DEFAULT_LANGUAGE
 from modeltranslation.utils import build_localized_fieldname
 
 
@@ -14,15 +15,33 @@ def activate_languages(languages: Iterable[str] = AVAILABLE_LANGUAGES) -> Iterat
     activate(old)
 
 
-def set_nofallbacks(instance: Model, attr: str, value: Any) -> None:
-    local_fieldname = build_localized_fieldname(attr, get_language())
+def set_nofallbacks(instance: Model, attr: str, value: Any, **kwargs) -> None:
+    language = kwargs.pop("language", get_language())
+    local_fieldname = build_localized_fieldname(attr, language)
     if hasattr(instance, local_fieldname):
         attr = local_fieldname
     return setattr(instance, attr, value)
 
 
 def get_nofallbacks(instance: Model, attr: str, *args, **kwargs) -> Any:
-    local_fieldname = build_localized_fieldname(attr, get_language())
+    language = kwargs.pop("language", get_language())
+    local_fieldname = build_localized_fieldname(attr, language)
     if hasattr(instance, local_fieldname):
         attr = local_fieldname
     return getattr(instance, attr, *args, **kwargs)
+
+
+def url_lang_decompose(url):
+    data = {"url": url, "permalink": url, "language": DEFAULT_LANGUAGE}
+    result = re.match(f"^\/?({'|'.join(AVAILABLE_LANGUAGES)})?\/(.*)", url).groups()
+    if result and len(result) == 2:
+        data["language"] = result[0]
+        data["permalink"] = "/%s" % result[1]
+    return data
+
+
+def get_field_translations(instance: Model, field_name: str, *args, **kwargs):
+    return {
+        lang: get_nofallbacks(instance, field_name, language=lang, *args, **kwargs)
+        for lang in AVAILABLE_LANGUAGES
+    }
