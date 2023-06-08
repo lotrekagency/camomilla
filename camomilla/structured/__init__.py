651b134f408885f3746a7a0b782d68f8dbd66c06
bnznamco <gabriele.baldi.01@gmail.com>
2023-06-08 12:00:29 +0200

    feat: added StructuredJSONField descriptor

--- camomilla/structured/__init__.py
+++ camomilla/structured/__init__.py
@@ -2,13 +2,24 @@ from typing import Any
 from .fields import *
 from .models import *
 from django.db.models import JSONField
+from django.db.models.query_utils import DeferredAttribute
+
+
+class StructuredDescriptior(DeferredAttribute):
+    def __set__(self, instance, value):
+        if isinstance(value, dict):
+            value = self.field.schema(**value)
+        value.validate()
+        instance.__dict__[self.field.attname] = value
 
 
 class StructuredJSONField(JSONField):
     # TODO: rewrite descriptor class to do always return a schema obj or null
     # TODO: share cache in querysets of models having this same field
     # TODO: write queries for prefetch related for models inside the field
-    
+
+    descriptor_class = StructuredDescriptior
+
     def __init__(self, schema, *args: Any, **kwargs: Any) -> None:
         self.schema = schema
         return super().__init__(*args, **kwargs)
