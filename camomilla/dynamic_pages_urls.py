d6363a54ecd7bb883e7337ffde352e55fe07805c
bnznamco <gabriele.baldi.01@gmail.com>
2023-06-10 13:28:20 +0200

    fix: fix page status check

--- camomilla/dynamic_pages_urls.py
+++ camomilla/dynamic_pages_urls.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from django.urls import path
 
-from .models import Page
+from .models import AbstractPage
 
 
 def fetch(request, *args, **kwargs):
     if "permalink" in kwargs:
-        page = Page.get_or_404(request)
+        page = AbstractPage.get_or_404(request)
     else:
-        page, _ = Page.get_or_create_homepage()
+        page, _ = AbstractPage.get_or_create_homepage()
     return render(request, page.template_name, {"page": page, "page_extra": {"class": page.__class__.__name__, "module": page.__module__} })
 
 
