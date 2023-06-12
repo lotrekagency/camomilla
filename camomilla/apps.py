5e770f3f367f2e404b56c8c5d4469b816218b901
bnznamco <gabriele.baldi.01@gmail.com>
2023-06-12 22:22:46 +0200

    feat: removed migrations and added MIGRATION_MODULES setting injection

--- camomilla/apps.py
+++ camomilla/apps.py
@@ -1,8 +1,15 @@
 from __future__ import unicode_literals
 
 from django.apps import AppConfig
+from django.conf import settings
 
 
 class CamomillaConfig(AppConfig):
-    default_auto_field = 'django.db.models.AutoField'
+    default_auto_field = "django.db.models.AutoField"
     name = "camomilla"
+
+    def ready(self):
+        migration_modules = getattr(settings, "MIGRATION_MODULES", {})
+        if "camomilla" not in migration_modules:
+            migration_modules["camomilla"] = "camomilla_migrations"
+        setattr(settings, "MIGRATION_MODULES", migration_modules)
