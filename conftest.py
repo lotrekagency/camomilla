b4b21cf1fe0f9193b5da9336c06b23aba09e5927
bnznamco <gabriele.baldi.01@gmail.com>
2023-09-19 09:03:20 +0200

    chore: fix test environment

--- conftest.py
+++ conftest.py
@@ -1,23 +1,47 @@
 import os
 from django.conf import settings
-
+import shutil
 from django.core.management import call_command
 from django.db import connections
 import pytest
+from devtools import debug
 
-# # UNCOMMENT THIS TO TEST WITH A LOCAL SQLITE DB FIXTURE
 
-# @pytest.fixture(scope='session')
-# def django_db_setup(django_db_blocker):
-#     from django.conf import settings
-#     test_db = 'test_db'
-#     settings.DATABASES['default']['NAME'] = test_db
+def clean_migration_folders():
+    from django.conf import settings
 
-#     with django_db_blocker.unblock():
-#         call_command('migrate', '--noinput')
-#     yield
-#     for connection in connections.all():
-#         connection.close()
+    for dir in settings.MIGRATION_MODULES.values():
+        debug("Removing", dir, os.path.exists(dir))
+        if os.path.exists(dir):
+            shutil.rmtree(dir)
+
+
+def create_migration_folders():
+    from django.conf import settings
+
+    for dir in settings.MIGRATION_MODULES.values():
+        debug("Creating", dir, os.path.exists(dir))
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+            open(os.path.join(dir, "__init__.py"), "w").close()
+
+
+@pytest.fixture(scope="session")
+def django_db_setup(django_db_blocker):
+    from django.conf import settings
+
+    create_migration_folders()
+    test_db = "test_db"
+    settings.DATABASES["default"]["NAME"] = test_db
+
+    with django_db_blocker.unblock():
+        call_command("makemigrations", interactive=False)
+        call_command("migrate", interactive=False)
+
+    yield
+    for connection in connections.all():
+        connection.close()
+    # clean_migration_folders()
 
 
 settings.configure(
@@ -33,7 +57,10 @@ settings.configure(
         "django.contrib.contenttypes",
         "django.contrib.auth",
         "camomilla",
+        "rest_framework",
         "rest_framework.authtoken",
+        "camomilla.theme",
+        "modeltranslation",
     ],
     ROOT_URLCONF="tests.urls",
     LANGUAGE_CODE="it",
