f4483fc883cb937b1762a6a309e83b99fa366c03
bnznamco <gabriele.baldi.01@gmail.com>
2023-09-19 11:23:21 +0200

    chore: added locale middleware, to example app settings

--- example/camomilla_example/settings.py
+++ example/camomilla_example/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'django.middleware.locale.LocaleMiddleware',
 ]
 
 ROOT_URLCONF = "example.camomilla_example.urls"
