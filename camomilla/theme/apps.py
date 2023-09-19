773af990380e2b1cdcd8000050b829db083ec893
bnznamco <gabriele.baldi.01@gmail.com>
2023-09-19 11:38:27 +0200

    fix: fix camomilla.theme bootstrap

--- camomilla/theme/apps.py
+++ camomilla/theme/apps.py
@@ -5,29 +5,37 @@ from django.apps import AppConfig, apps
 from django.conf import settings
 
 
+def add_apps(*app_list):
+    installed_apps = getattr(settings, "INSTALLED_APPS", [])
+    changed = False
+    for app in app_list:
+        if app not in installed_apps:
+            changed = True
+            installed_apps = [app, *installed_apps]
+    if changed:
+        setattr(settings, "INSTALLED_APPS", installed_apps)
+        apps.app_configs = OrderedDict()
+        apps.apps_ready = apps.models_ready = apps.loading = apps.ready = False
+        apps.clear_cache()
+        apps.populate(settings.INSTALLED_APPS)
+
+
+def set_default_settings(**kwargs):
+    for key, value in kwargs.items():
+        setattr(settings, key, getattr(settings, key, value))
+
+
 class CamomillaThemeConfig(AppConfig):
     name = "camomilla.theme"
 
     def ready(self):
-        installed_apps = getattr(settings, "INSTALLED_APPS", [])
-        changed = False
-        if "django_jsonform" not in installed_apps:
-            changed = True
-            installed_apps = ["django_jsonform", *installed_apps]
-        if "admin_interface" not in installed_apps:
-            changed = True
-            installed_apps = ["admin_interface", *installed_apps]
-        if "colorfield" not in installed_apps:
-            changed = True
-            installed_apps = ["colorfield", *installed_apps]
-        if changed:
-            setattr(settings, "INSTALLED_APPS", installed_apps)
-            apps.app_configs = OrderedDict()
-            apps.apps_ready = apps.models_ready = apps.loading = apps.ready = False
-            apps.clear_cache()
-            apps.populate(settings.INSTALLED_APPS)
-        setattr(
-            settings,
-            "X_FRAME_OPTIONS",
-            getattr(settings, "X_FRAME_OPTIONS", "SAMEORIGIN"),
+        set_default_settings(
+            CKEDITOR_UPLOAD_PATH="editor-uploads/", X_FRAME_OPTIONS="SAMEORIGIN"
+        )
+        add_apps(
+            "ckeditor_uploader",
+            "ckeditor",
+            "django_jsonform",
+            "admin_interface",
+            "colorfield",
         )
