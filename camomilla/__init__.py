54f38685cc0d69bc33304138c3e92998269d134f
bnznamco <gabriele.baldi.01@gmail.com>
2023-09-25 11:42:14 +0200

    chore: fix versioning

--- camomilla/__init__.py
+++ camomilla/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "6.0.0"
+__version__ = "5.8.5"
 
 
 def get_core_apps():
--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup
 
-__version__ = "6.0.0"
+__version__ = "5.8.5"
 
 setup(version=__version__)
