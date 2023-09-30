01914bddcc13903917c71b7091e0b7d0dc454e70
bnznamco <gabriele.baldi.01@gmail.com>
2023-09-30 04:21:34 +0200

    fix: fix camomilla context init

--- camomilla/apps.py
+++ camomilla/apps.py
@@ -3,14 +3,14 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 from django.conf import settings
 
-from camomilla.context.autodiscover import autodiscover_context_files
-
 
 class CamomillaConfig(AppConfig):
     default_auto_field = "django.db.models.AutoField"
     name = "camomilla"
 
     def ready(self):
+        from camomilla.context.autodiscover import autodiscover_context_files
+
         migration_modules = getattr(settings, "MIGRATION_MODULES", {})
         if "camomilla" not in migration_modules:
             migration_modules["camomilla"] = "camomilla_migrations"
